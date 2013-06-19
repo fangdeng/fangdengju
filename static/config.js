dd975cd101c4db19c9630c9414d3c3530b75d08a
xiaoji121 <dong.fly.ming@gmail.com>
2013-06-19 19:27:40 +0800

    ff

--- static/config.js
+++ static/config.js
@@ -5,13 +5,13 @@ seajs.config({
     // Configure alias
     alias: {
         'jquery': {
-            src: '/static/lib/jquery-1.9.1.min.js',
+            src: './static/lib/jquery-1.9.1.min.js',
             exports: 'jQuery'
         },
-        'bootstrap': '/static/bootstrap/js/bootstrap.min.js',
-        'jqueryui': '/static/lib/jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.min.js',
-        'mousewheel': '/static/scrollbar/jquery.mousewheel.js',
-        'scrollbar': '/static/scrollbar/perfect-scrollbar.js'
+        'bootstrap': '../../bootstrap/js/bootstrap.min.js',
+        'jqueryui': '../../lib/jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.min.js',
+        'mousewheel': '../../scrollbar/jquery.mousewheel.js',
+        'scrollbar': '../../scrollbar/perfect-scrollbar.js'
     },
     debug: true
 });
